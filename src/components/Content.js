6f1fcf28a005b321c989df4978f0efbc55afc417
Deepika Choudhary <deepika.choudhary@in-sciencelabs.com>
2022-08-18 12:01:30 +0530

    intial commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
+import { ListItemSecondaryAction } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
-// import { baseURL, fetchApi } from "./fetchApi";
 
 const options = {
   method: "GET",
@@ -9,59 +9,76 @@ const options = {
   },
 };
 
-// const url =
-//   "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200";
-
-fetch(
-  "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 const Content = () => {
-  // async function postData() {
-  //   const res = await fetch(
-  //     "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         "X-RapidAPI-Key":
-  //           "0e5c9272f8msh0c40a6f7a67cc9ap187953jsn1fe00a2e87ff",
-  //         "X-RapidAPI-Host": "real-estate-usa.p.rapidapi.com",
-  //       },
-  //       body: JSON.stringify(postData),
-  //     }
-  //   );
-  //   const data = await res.json();
-  //   console.log(data);
-  // }
-  // postData();
+  const [details, SetDetails] = useState(null);
+  async function postData() {
+    const res = await fetch(
+      "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "0e5c9272f8msh0c40a6f7a67cc9ap187953jsn1fe00a2e87ff",
+          "X-RapidAPI-Host": "real-estate-usa.p.rapidapi.com",
+        },
+        body: JSON.stringify(postData),
+      }
+    );
+    const data = await res.json();
+    console.log(data.properties);
+    SetDetails(data.properties);
+    console.log(details);
+  }
+  useEffect(() => {
+    postData();
+  }, []);
 
   return (
     <div className="flex">
-      <div className="border-black border-2 p-3 ml-20 m-10 w-25 h-40">
-        <img src="" />
-        <h3 className="font-bold text-purple-500">$2095/month</h3>
-        <h3 className="font-medium">Palm Harbor</h3>
-        <h3 className="font-light text-gray-500">Location</h3>
-        <p>3 bedroom 2 bathroom area</p>
-      </div>
+      {/* //   <div className="border-black border-2 p-3 ml-20 m-10 w-25 h-40">
+    //     <img src={items.listings[0].photos[0].href} />
+    //     <h3 className="font-bold text-purple-500">
+    //       ${items.listings[0].price}
+    //     </h3>
+    //     <h3 className="font-medium">Product Name {items.product_name}</h3>
+    //     <h3 className="font-light text-gray-500">{items.listing[0].address}</h3>
+    //     <p>
+    //       {items.listings[0].beds} bedroom {items.listings[0].baths_full}{" "}
+    //       bathroom {items.listings[0].raw_products.sqft} sqft area
+    //     </p>
+    //   </div> */}
+
+      {details ? (
+        <div className="grid grid-rows-5 grid-flow-col">
+          {details.map((item) => {
+            return (
+              <div className="flex">
+                <div className="border-black border-2 p-4 ml-20 mt-10 w-full h-50">
+                  {/* <img src={item.listings[0].photo.href} /> */}
+                  <h3 className="font-bold text-purple-500">
+                    ${item.listings[0].price}
+                  </h3>
+                  <h3 className="font-medium">
+                    Product Name {item.product_name}
+                  </h3>
+                  <h3 className="font-light text-gray-500">
+                    {item.listings[0].address.city}
+                  </h3>
+                  <p>
+                    {item.listings[0].beds} bedroom
+                    {item.listings[0].baths_full}
+                    bathroom {item.listings[0].sqft} sqft area
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        "No results found"
+      )}
     </div>
   );
 };
-
-// export async function getStaticProps() {
-//     const propertyForSale = await fetchApi(
-//     `${baseURL}/api/v1/properties?postal_code=94105&offset=0&limit=200`
-//   );
-
-//   return {
-//     props: {
-//       propertiesForSale: propertyForSale?.hits,
-//     },
-//   };
-// }
 
 export default Content;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,47 @@
-import { React, useState } from "react";
-// import { FormControl, Input, FormLabel } from "@material-ui/core";
+import { React, useEffect, useState } from "react";
 
 const Search = () => {
+  const [data, setData] = useState([]);
+  const [filterVal, setFilterVal] = useState("");
+  const [searchApiData, setSearchApiData] = useState([]);
+  async function postData() {
+    const res = await fetch(
+      "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "0e5c9272f8msh0c40a6f7a67cc9ap187953jsn1fe00a2e87ff",
+          "X-RapidAPI-Host": "real-estate-usa.p.rapidapi.com",
+        },
+        body: JSON.stringify(postData),
+      }
+    );
+    const data = await res.json();
+    console.log(data.properties);
+    setData(data.properties);
+    console.log(data);
+  }
+  useEffect(() => {
+    postData();
+  }, []);
+  const handleFilter = (e) => {
+    if (e.target.value == "") {
+      setData(searchApiData);
+    } else {
+      const filterResult = searchApiData.filter((item) =>
+        item.listings[0].price.address.city
+          .toLowerCase()
+          .includes(e.target.value.toLowerCase())
+      );
+      if (filterResult.length > 0) {
+        setData(filterResult);
+      } else {
+        setData([{ price: "No data" }]);
+      }
+      setFilterVal(e.target.value);
+    }
+  };
   return (
     <div className="ml-20">
       <div className="grid gap-1 mb-2 md:grid-cols-2">
@@ -24,7 +64,6 @@ const Search = () => {
               </label>
               <input
                 type="text"
-                id="location"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Location"
               />
@@ -39,7 +78,6 @@ const Search = () => {
               </label>
               <input
                 type="text"
-                id="When"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="When"
               />
@@ -53,10 +91,10 @@ const Search = () => {
                 Price
               </label>
               <input
-                type="text"
-                id="Price"
+                type="search"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Price"
+                onInput={(e) => handleFilter(e)}
               />
             </div>
 
@@ -69,7 +107,6 @@ const Search = () => {
               </label>
               <input
                 type="text"
-                id="Property Type"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Property Type"
               />
@@ -85,3 +122,113 @@ const Search = () => {
 };
 
 export default Search;
+
+// import React, { useState, useEffect } from "react";
+// import { Card, Input } from "@material-ui/core";
+// export default function Posts() {
+//   const [APIData, setAPIData] = useState(null);
+//   const [filteredResults, setFilteredResults] = useState([]);
+//   const [searchInput, setSearchInput] = useState("");
+
+//   // const [details, SetDetails] = useState(null);
+//   async function postData() {
+//     const res = await fetch(
+//       "https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200",
+//       {
+//         method: "GET",
+//         headers: {
+//           "X-RapidAPI-Key":
+//             "0e5c9272f8msh0c40a6f7a67cc9ap187953jsn1fe00a2e87ff",
+//           "X-RapidAPI-Host": "real-estate-usa.p.rapidapi.com",
+//         },
+//         body: JSON.stringify(postData),
+//       }
+//     );
+//     const data = await res.json();
+//     console.log(data.properties);
+//     setAPIData(data.properties);
+//     console.log(APIData);
+//   }
+//   useEffect(() => {
+//     //   axios.get(`https://jsonplaceholder.typicode.com/users`).then((response) => {
+//     //     setAPIData(response.data);
+//     //   });
+//     // }, []);
+
+//     postData();
+//   }, []);
+
+//   const searchItems = (searchValue) => {
+//     setSearchInput(searchValue);
+//     if (searchInput !== "") {
+//       const filteredData = APIData.filter((item) => {
+//         return Object.values(item)
+//           .join("")
+//           .toLowerCase()
+//           .includes(searchInput.toLowerCase());
+//       });
+//       setFilteredResults(filteredData);
+//     } else {
+//       setFilteredResults(APIData);
+//     }
+//   };
+
+//   return (
+//     <div style={{ padding: 20 }}>
+//       <Input
+//         icon="search"
+//         placeholder="Search..."
+//         onChange={(e) => searchItems(e.target.value)}
+//       />
+//       {/* <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}> */}
+//       {searchInput.length > 1
+//         ? filteredResults.map((item) => {
+//             return (
+//               <div className="flex">
+//                 <div className="border-black border-2 p-4 ml-20 mt-10 w-full h-50">
+//                   {/* <img src={item.listings[0].photo.href} /> */}
+//                   <h3 className="font-bold text-purple-500">
+//                     ${item.listings[0].price}
+//                   </h3>
+//                   <h3 className="font-medium">
+//                     Product Name {item.product_name}
+//                   </h3>
+//                   <h3 className="font-light text-gray-500">
+//                     {item.listings[0].address.city}
+//                   </h3>
+//                   <p>
+//                     {item.listings[0].beds} bedroom
+//                     {item.listings[0].baths_full}
+//                     bathroom {item.listings[0].sqft} sqft area
+//                   </p>
+//                 </div>
+//               </div>
+//             );
+//           })
+//         : APIData.map((item) => {
+//             return (
+//               <div className="flex">
+//                 <div className="border-black border-2 p-4 ml-20 mt-10 w-full h-50">
+//                   {/* <img src={item.listings[0].photo.href} /> */}
+//                   <h3 className="font-bold text-purple-500">
+//                     ${item.listings[0].price}
+//                   </h3>
+//                   <h3 className="font-medium">
+//                     Product Name {item.product_name}
+//                   </h3>
+//                   <h3 className="font-light text-gray-500">
+//                     {item.listings[0].address.city}
+//                   </h3>
+//                   <p>
+//                     {item.listings[0].beds} bedroom
+//                     {item.listings[0].baths_full}
+//                     bathroom {item.listings[0].sqft} sqft area
+//                   </p>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//       {/* // </Card.Group> */}
+//     </div>
+//   );
+// }
